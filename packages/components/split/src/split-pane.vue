dc4aa910bda7a393bbc00903d190ba2bc391275f
Dsaquel <noblet.ouwaysgta5@gmail.com>
2025-02-12 21:55:07 +0100

    chore: wip

--- packages/components/split/src/split-pane.vue
+++ packages/components/split/src/split-pane.vue
@@ -1,13 +1,19 @@
 <template>
-  <div ref="paneRef" :class="[ns.b()]" :style="paneStyle">
+  <div
+    ref="paneRef"
+    :class="[
+      ns.b(),
+      { 'is-vertical': splitContext?.props.direction === 'vertical' },
+    ]"
+    :style="paneStyle"
+  >
     <div class="PANE CONTENT">
       <slot />
     </div>
     <div
       class="SPLITTER"
       :style="lineStyle"
-      draggable="true"
-      @dragstart="(e) => splitContext.dragStart(e, index)"
+      @mousedown.prevent="(e) => splitContext.dragStart(e, index)"
     />
   </div>
 </template>
@@ -66,27 +72,31 @@ const setIndex = (val: number) => {
 }
 
 const paneStyle = computed<CSSProperties>(() => {
+  if (!splitContext.containerRef.value) return {}
   return {
-    [splitContext.props.direction === 'vertical' ? 'height' : 'width']: `${
-      size.value * 100
-    }%`,
+    [splitContext.props.direction === 'vertical'
+      ? 'height'
+      : 'width']: `${size.value}px`,
   }
 })
 
+const fixToPrecision = (value: number) => Number.parseFloat(value.toFixed(6))
 const calcSplitter = () => {
   const style: CSSProperties = {
-    borderStyle: 'solid',
-    borderColor: 'black',
-    cursor: 'ew-resize	',
+    backgroundColor: 'black',
+    cursor:
+      splitContext.props.direction === 'vertical' ? 'ns-resize' : 'ew-resize',
+    width: splitContext.panes.value.length > index.value + 1 ? '3px' : 0,
   }
-  style.borderWidth =
-    splitContext.panes.value.length > index.value + 1 ? '3px' : 0
-  size.value = 1 / splitContext.panes.value.length
+
+  size.value =
+    splitContext.containerRef.value!.offsetWidth *
+    fixToPrecision(1 / splitContext.panes.value.length)
   lineStyle.value = style
 }
 
 const splitPaneState = reactive({
-  element: paneRef.value!,
+  element: computed(() => paneRef.value!),
   uid: currentInstance!.uid,
   setIndex,
   size,
--- packages/components/split/src/split.vue
+++ packages/components/split/src/split.vue
@@ -1,10 +1,7 @@
 <template>
   <div
     ref="containerRef"
-    :class="[ns.b()]"
-    @mousemove="dragMove"
-    @mouseup="dragEnd"
-    @mouseleave="dragEnd"
+    :class="[ns.b(), { 'is-vertical': direction === 'vertical' }]"
   >
     <slot />
   </div>
@@ -12,6 +9,7 @@
 
 <script lang="ts" setup>
 import { getCurrentInstance, provide, ref, shallowRef, watch } from 'vue'
+import { clamp } from 'lodash-unified'
 import { useNamespace, useOrderedChildren } from '@element-plus/hooks'
 import { splitContextKey } from './types'
 import { splitProps } from './split'
@@ -32,6 +30,7 @@ const {
 } = useOrderedChildren<SplitPaneState>(getCurrentInstance()!, 'ElSplitPane')
 
 const dragging = ref(false)
+const dragState = ref({})
 
 watch(panes, () => {
   panes.value.forEach((instance: SplitPaneState, index: number) => {
@@ -39,34 +38,183 @@ watch(panes, () => {
   })
 })
 
-let startPosition = 0
-const startSplit = 0
+const startPosition = 0
 const currentIndex = ref(-1)
 
-function dragMove(e: MouseEvent) {
-  if (containerRef.value && dragging.value) {
+const fixToPrecision = (value: number) => Number.parseFloat(value.toFixed(6))
+const MIN = 30
+const MAX = 1
+const BASE_SHIFT = 6
+
+const onMousemove = (e: MouseEvent) => {
+  const currIndex = currentIndex.value
+  if (containerRef.value && dragging.value && currIndex >= 0) {
     const position = props.direction === 'vertical' ? e.pageY : e.pageX
+    const isIncrease = position > startPosition
     const totalSize =
       props.direction === 'vertical'
         ? containerRef.value.offsetHeight
         : containerRef.value.offsetWidth
-    const dp = position - startPosition
-    panes.value[currentIndex.value].size =
-      startSplit + +((dp / totalSize) * 100).toFixed(2)
+    if (position === startPosition || !position) return
+
+    const sizeLeft = panes.value
+      .slice(0, currIndex)
+      .reduce((prev, curr) => prev + curr.size, BASE_SHIFT / totalSize)
+
+    const incr = fixToPrecision(position / totalSize - sizeLeft)
+
+    const prevVal = panes.value[currIndex].size
+    const currVal = clamp(incr, MIN, MAX)
+
+    let ratioToDispatch = prevVal - incr
+    if (!ratioToDispatch) return
+    //console.log('first', ratioToDispatch)
+    let needToPush = currVal === MIN
+    let idx = currIndex
+    const doLayout = () => {
+      const currPanel = panes.value[idx]
+      const sum = fixToPrecision(currPanel.size + ratioToDispatch)
+      const clampedValue = clamp(sum, MIN, MAX)
+      currPanel.size = clampedValue
+      const rest = sum - clampedValue
+      const clamped = clampedValue === MIN
+      return { rest, clamped }
+    }
+
+    if (!needToPush) {
+      console.log(' ???? ??', currVal)
+      panes.value[currIndex].size = currVal
+      idx += 1
+      const { clamped } = doLayout()
+      if (isIncrease && clamped) {
+        needToPush = true
+      }
+    }
+
+    if (needToPush && ratioToDispatch) {
+      //console.log('huh ?', ratioToDispatch)
+      if (!isIncrease) {
+        idx = currentIndex.value
+      }
+
+      while (
+        idx > 0 &&
+        idx < panes.value.length &&
+        (needToPush || ratioToDispatch !== 0)
+      ) {
+        if (isIncrease) {
+          ratioToDispatch = -Math.abs(ratioToDispatch)
+          idx++
+          console.log('passe la', idx)
+        } else {
+          console.log('ici ?')
+          ratioToDispatch = -Math.abs(ratioToDispatch)
+          idx--
+        }
+        //console.log('looping')
+        //console.log(idx)
+        const { rest, clamped } = doLayout()
+        needToPush = clamped
+        ratioToDispatch = rest
+      }
+    }
   }
 }
 
-function dragStart(e: MouseEvent, index: number) {
-  currentIndex.value = index
-  dragging.value = true
-  startPosition = props.direction === 'vertical' ? e.pageY : e.pageX
-  //startSplit = split.value
-}
-
-function dragEnd() {
+/* eslint-disable @typescript-eslint/no-unused-vars */
+const onMouseup = () => {
   dragging.value = false
   currentIndex.value = -1
+  //startPosition = 0
+  window.removeEventListener('mousemove', onMousemove)
+  window.removeEventListener('mouseup', onMouseup)
+  window.removeEventListener('mouseleave', onMouseup)
 }
+/* eslint-enable @typescript-eslint/no-unused-vars */
+const onMousedown = (e: MouseEvent, index: number) => {
+  dragging.value = true
+  const paneState = panes.value[index]
+  const elementRect = paneState.element.getBoundingClientRect()
+  const containerRect = containerRef.value!.getBoundingClientRect()
+  dragState.value = {
+    startMouseLeft: e.clientX,
+    startLeft: elementRect.right - containerRect.left,
+    startPaneLeft: elementRect.left - containerRect.left,
+    containerLeft: containerRect.left,
+  }
+  console.log(dragState.value)
+  const handleMouseMove = (e: MouseEvent) => {
+    const deltaLeft = e.clientX - (dragState.value as any).startMouseLeft
+    const newSize =
+      (dragState.value as any).startLeft +
+      deltaLeft -
+      (dragState.value as any).startPaneLeft
+    const incr = newSize - paneState.size
+    //console.log('newSize', newSize)
+    const rawLeft = paneState.size + incr
+    const rawRight = panes.value[index + 1].size - incr
+    if (incr === 0) return
+
+    const isIncrease = incr > 0
+    const leftSize = Math.max(MIN, rawLeft)
+    const rightSize = Math.max(MIN, rawRight)
+    const getShift = () => {
+      if (isIncrease) {
+        if (rawLeft === MIN) {
+          console.log('huh ????')
+          return incr
+        }
+        const resizeRest = rightSize - rawRight
+        return resizeRest
+      } else {
+        if (rawLeft === MIN) {
+          console.log('decreasqe')
+          return incr
+        }
+        const resizeRest = leftSize - rawLeft
+        return resizeRest
+      }
+    }
+    const shift = getShift()
+
+    paneState.size = leftSize
+    panes.value[index + 1].size = rightSize
+
+    if (shift !== 0) doPushPanes(isIncrease, shift)
+  }
+  const handleMouseUp = () => {
+    document.removeEventListener('mousemove', handleMouseMove)
+    document.removeEventListener('mouseup', handleMouseUp)
+  }
+  const doPushPanes = (isIncrease: boolean, shift: number) => {
+    console.log('enter in do push panes with shift=  ', shift)
+    let idx = isIncrease ? index + 1 : index
+
+    while (idx > 0 && idx < panes.value.length - 1 && shift !== 0) {
+      if (isIncrease) {
+        //console.log('increase')
+        idx++
+        shift = Math.abs(shift)
+      } else {
+        //console.log('decrease')
+        idx--
+        shift = Math.abs(shift)
+      }
+      const currPaneState = panes.value[idx]
+      const rawSize = currPaneState.size - shift
+      const paneSize = Math.max(MIN, rawSize)
+      //console.log('newSize', rawSize)
+      currPaneState.size = paneSize
+      shift = paneSize - rawSize
+    }
+  }
+  document.addEventListener('mousemove', handleMouseMove)
+  document.addEventListener('mouseup', handleMouseUp)
+}
+
+//window.onmousemove = (ev) => {
+//  console.log(ev.clientX)
+//}
 
 provide(splitContextKey, {
   containerRef,
@@ -75,18 +223,10 @@ provide(splitContextKey, {
   props,
   removePane,
   dragging,
-  dragStart,
+  dragStart: onMousedown,
 })
 
 defineExpose({
   //
 })
 </script>
-
-<style lang="css" scoped>
-.dragger {
-  width: 10px;
-  background-color: blue;
-  cursor: ew-resize;
-}
-</style>
